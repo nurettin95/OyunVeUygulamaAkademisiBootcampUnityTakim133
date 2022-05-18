b70f70571b609efbd891b79fe2fe311693646c07
Nurettin <nurettin1995@hotmail.com>
2022-05-18 16:47:33 +0300

    add

--- WisdomRunner/Assets/Scripts/ItemCollector.cs
+++ WisdomRunner/Assets/Scripts/ItemCollector.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.UI;
 
 public class ItemCollector : MonoBehaviour
@@ -8,7 +9,7 @@ public class ItemCollector : MonoBehaviour
 
     int puan = 0;
     
-    [SerializeField] Text puanText;
+    [SerializeField] TextMeshProUGUI puanText;
 
     private void OnTriggerEnter(Collider other)
     {
