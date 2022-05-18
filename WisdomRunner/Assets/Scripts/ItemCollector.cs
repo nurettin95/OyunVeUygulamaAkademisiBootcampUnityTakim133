3b8f60b8631eaeb03d4eda416dccf08ac427e705
Nurettin <nurettin1995@hotmail.com>
2022-05-18 14:56:55 +0300

    Assetler düzenlendi puan toplama scripti yazıldı

---
+++ WisdomRunner/Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+
+    int puan = 0;
+    
+    [SerializeField] Text puanText;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Book"))
+        {
+            Destroy(other.gameObject);
+            puan++;
+            puanText.text = "Puan: " + puan;
+        }
+
+        if(other.gameObject.CompareTag("eksiPuan"))
+        {
+            Destroy(other.gameObject);
+            puan--;
+            puanText.text = "Puan: " + puan;
+        }
+    }
+}
